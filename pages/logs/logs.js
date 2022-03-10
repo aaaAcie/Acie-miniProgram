4151a57a65dc6c0e4ea59a1fff91204998561a21
aaaAcie <913587892@qq.com>
2022-03-10 21:39:00 +0800

    修改了日志页面，待后续使用好玩的api去迭代

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -11,13 +11,13 @@ Page({
     })
   },
   onLoad() {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
-      })
-    })
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(log => {
+    //     return {
+    //       date: util.formatTime(new Date(log)),
+    //       timeStamp: log
+    //     }
+    //   })
+    // })
   }
 })
